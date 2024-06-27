5beb87cdf573d9f867e80f900d5fb468d38e9f14
Jared <jaredozono@gmail.com>
2024-06-26 18:54:52 -0700

    ortografia corregida

--- src/Components/body/index.jsx
+++ src/Components/body/index.jsx
@@ -3,13 +3,13 @@ import React from "react"
 const projects = [ 
     {
         title: 'E-commerce fake API Platzi',
-        description: 'Tienda en línea con React, Vite.js y Tailwind CSS. Con esta aplicación aprendí a manipular una API y utilizar sus datos y redirigir al usuario según donde están haciendo click',
+        description: 'Tienda en línea con React, Vite.js y Tailwind CSS. Con esta aplicación, aprendí a manipular una API, utilizar sus datos y redirigir al usuario según donde hagan clic',
         image: '/public/image/E-commerce.png',
         link: 'https://adorable-vacherin-8c1dae.netlify.app/',
     },
     {
         title: 'To do list',
-        description: 'Una aplicación para anotar las cosas por hacer en el día usando React.js. Con el desarrollo de esta página aprendí a hacer persistencia de datos usando el local storage',
+        description: 'Una aplicación para anotar las tareas del día usando React.js. Con el desarrollo de esta página, aprendí a implementar persistencia de datos usando el local storage',
         image: '/public/image/To-do-list.png',
         link: 'https://chatojared06.github.io/TodoMachine/',
     },
@@ -17,14 +17,14 @@ const projects = [
 
 const projects2 = [ 
     {
-        title: 'Concepto basico de un E-commerce sin logica ',
-        description: 'En este proyecto aprendi conceptos bacicos de HTML y CSS.',
+        title: 'Concepto básico de un E-commerce sin lógica',
+        description: 'En este proyecto aprendí conceptos básicos de HTML y CSS',
         image: '/public/image/ecommerce-boseto.png',
         link: 'https://chatojared06.github.io/curso-frontend-developer-JavaScript-practico/',
     },
     {
-        title: 'To do list',
-        description: 'En este pryecto basico aprendi a llamar los ultimos videos de un canal de youtube y precentarlos en la aplicacion. ',
+        title: 'Boseto de un Portafolio profesional y página de contacto',
+        description: 'En este proyecto básico aprendí a llamar los últimos videos de un canal de YouTube y presentarlos en la aplicación',
         image: '/public/image/jedi.png',
         link: 'https://chatojared06.github.io/async-landing/',
     },
@@ -34,7 +34,7 @@ const Body = () => {
 return(
     <div className="flex flex-col">
         <div className="justify-center mt-6"> 
-        <p className="text-white text-center text-2xl font-poppins"> Proyectos con Lógica </p>
+        <p className="text-white text-center text-2xl font-poppins"> Proyectos con lógica </p>
         </div>
         
             <div className="flex flex-wrap justify-center sm:grid-cols-2 md:grid-cols-3 gap-24 px-4 py-10">
@@ -52,7 +52,7 @@ return(
                 rel="noopener noreferrer"
                 className="inline-block bg-cyan-800 text-white w-full px-3 py-2 rounded-lg text-lg font-semibold text-center font-poppins"
                 >
-                Ver Proyecto
+                Ver proyecto
                 </a>
                 </div>
                 </div>
@@ -60,7 +60,7 @@ return(
         </div>
 
         <div className="justify-center mt-6 "> 
-        <p className="text-white text-center text-2xl font-poppins"> Proyectos con Html, CSS y js basicos </p>
+        <p className="text-white text-center text-2xl font-poppins"> Proyectos con HTML, CSS y JavaScript básicos </p>
         </div>
         
             <div className="flex flex-wrap justify-center sm:grid-cols-2 md:grid-cols-3 gap-24 px-4 py-10">
@@ -78,7 +78,7 @@ return(
                 rel="noopener noreferrer"
                 className="inline-block bg-cyan-800 text-white w-full px-3 py-2 rounded-lg text-lg font-semibold text-center font-poppins"
                 >
-                Ver Proyecto
+                Ver proyecto
                 </a>
                 </div>
                 </div>
